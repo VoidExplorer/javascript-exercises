33a84fd39242af856c729f64d447bea7d3704cc1
VoidExplorer <161141243+VoidExplorer@users.noreply.github.com>
2024-08-06 19:32:57 +0300

    feat: solve js-02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, count) {
+    if (count<0) return "ERROR"
+    result = ""
+    for (let i = 0; i < count; i++) {
+        result +=text
+    }
+    return result
 };
 
 // Do not edit below this line
