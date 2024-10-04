ea02842ea686e05e49939525c05a174c5b91df0c
VoidExplorer <161141243+VoidExplorer@users.noreply.github.com>
2024-10-04 09:00:03 +0300

    feat: solve js-10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) return "OOPS";
+  const arr = [0, 1];
+  for (let i = 1; i < num; i++) {
+    arr.push(arr[i] + arr[i - 1]);
+  }
+  console.log(arr);
+  return arr[num];
 };
 
 // Do not edit below this line
