5e8035313665995001a178f80d45c77c2a53be49
VoidExplorer <161141243+VoidExplorer@users.noreply.github.com>
2024-10-04 08:38:51 +0300

    feat: solve js-08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, current) => {
+    total+=current;
+    return total;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, current) => {
+    total*=current;
+    return total;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+  return Math.pow(num, pow)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	fact = 1;
+  for (let i=num; i>1; i--) {
+    fact*= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
