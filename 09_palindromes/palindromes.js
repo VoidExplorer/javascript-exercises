39db177ff9c32dd0153d365ebf9e2fd81b36cde4
VoidExplorer <161141243+VoidExplorer@users.noreply.github.com>
2024-10-04 08:39:25 +0300

    feat: solve js-09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  return (
+    string.toLowerCase().split("").reverse().clean().join("") ===
+    string.toLowerCase().split("").clean().join("")
+  );
 };
 
+Array.prototype.clean = function () {
+  for (let i = 0; i < this.length; i++) {
+    item = this[i];
+    if (item == "," || item == "." || item == "!" || item == " ") {
+      this.splice(i, 1);
+      i--;
+    }
+  }
+
+  return this;
+};
 // Do not edit below this line
 module.exports = palindromes;
