58e1bd785351a635016c174f316ed6c7ef6626c8
VoidExplorer <161141243+VoidExplorer@users.noreply.github.com>
2024-10-04 09:19:56 +0300

    feat: solve js-12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.sort(
+    (a, b) =>
+      getAge(b.yearOfBirth, b.yearOfDeath) -
+      getAge(a.yearOfBirth, a.yearOfDeath)
+  )[0];
 };
 
+const getAge = function (yearOfBirth, yearOfDeath) {
+  if (!yearOfDeath) yearOfDeath = new Date().getFullYear();
+  console.log(yearOfDeath);
+  return yearOfDeath - yearOfBirth;
+};
 // Do not edit below this line
 module.exports = findTheOldest;
